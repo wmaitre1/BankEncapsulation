8f795a5f2c85d350c2f7b94e40bdda94b15a3f71
William Maitre <william.maitre@gmail.com>
2022-08-01 16:16:50 -0400

    initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+
+        private double balance = 0;
+
+        //for my reference. this is encapsulation
+       public void Deposit(double amount)
+        {
+            balance = amount;
+        }
+
+        //this how you define a method. important to remember
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+           
+            Console.WriteLine("Please enter your desired deposit amount");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            
+            account.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Please see your balance.  Your balance is {account.GetBalance()}");
+
+
+            Console.ReadLine();
+            
+
+        }
+
+    }
+}
+
+
+
+
